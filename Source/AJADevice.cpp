5651d585c12786fa6effcc6de87f7c90969c010b
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2025-06-23 15:31:10 +0300

    From nodos-1.3: Fix wrong firmware date is read from the register for Corvid-44 devices
    (cherry picked from commit 3a6cfab245743e0e7ef2ad6b9c4644d968231e3b)

--- Source/AJADevice.cpp
+++ Source/AJADevice.cpp
@@ -958,7 +958,20 @@ void AJADevice::UnregisterNode(nos::uuid id)
 bool AJADevice::CheckFirmware(std::string& msg, std::string& msgDetails)
 {
     std::string date, time;
-    auto re = GetRunningFirmwareDate(date, time);
+    BITFILE_INFO_STRUCT bitFileInfo{};
+    bitFileInfo.whichFPGA = eFPGAVideoProc;
+    if (DriverGetBitFileInformation(bitFileInfo))
+    {
+		date = bitFileInfo.dateStr;
+		time = bitFileInfo.timeStr;
+	}
+
+	if (date.empty() || time.empty())
+	{
+		msg = "Firmware date or time is not available.";
+		return false;
+	}
+
     std::string model = GetModelName();
     if (auto it = firmware_list.find(model); it != firmware_list.end())
     {
@@ -970,6 +983,8 @@ bool AJADevice::CheckFirmware(std::string& msg, std::string& msgDetails)
         }
         return true;
     }
+    // ? This returns the below message if a newer untested firmware is used. We should return this even if its newer, if not tested.
+    // Ideally a config file should be used to get the tested firmware list.
     msg = "Not tested firmware";
     msgDetails = "Firmware (" + date + ") for device (" + model + ") has not been tested.";
     return false;
